18f064cb3ad1202805e19c8575272c44afd21902
Dominik Schmidt <dev@dominik-schmidt.de>
2022-12-10 02:05:27 +0100

    Add router to demo setup issue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <router-link to="/vite"><img src="/vite.svg" class="logo" alt="Vite logo" /></router-link>
+    <router-link to="/vue"><img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /></router-link>
+
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import Vue, { h } from 'vue'
+import Vue from 'vue'
 import './style.css'
 import App from './App.vue'
+import VitePage from './components/VitePage.vue'
+import VuePage from './components/VuePage.vue'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/vite', component: VitePage },
+  { path: '/vue', component: VuePage }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   'el': '#app',
+  router,
   render: (h: any) => h(App)
 })
